0c21acfe57cb29816c2feeeb4dd65d061b7d61fc
moya <rafusanjanimoya@gmail.com>
2021-08-10 09:01:28 +0300

    hjkl

---
+++ home.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<?php 
+
+require 'config.php';
+ ?>
+
+<html>
+<head>
+	<meta charset="utf-8">
+		<title>PURGE TRANSPORTATION AND LOGISTICS</title>
+		<link rel="stylesheet" type="text/css" href="css/app.css">
+	
+
+<body >
+	 <?php include 'header.php'; ?>
+	
+<main class="py-4">
+        <div class="container">
+
+        	<h2>PURGE TRANSPORTATION AND LOGISTICS LTD</h2>
+        	<h3 align ="center"><font color ="blue" face = arial, helvetica, cambia> WELCOME </font>
+
+
+        		<p>Purge Transportation and logistics offer  transportation services to all parts of the country.
+        	<div class="row">
+	        		 
+	        		 <div class="col-md-6 col-sm-12">
+
+	        		 	<img src="images/bus1.JPG" width="100px" height="100px">
+
+	        		 	<img src="images/bus2.JPG" width="100px" height="100px">
+
+	        		 	
+
+	        		 	<img src="images/purge.JPG" width="100px" height="100px">
+	        		 	<img src="images/purge2.JPG" width="100px" height="100px">
+
+
+	        		 	<hr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	        		 	<div id="footer">
+	<h4> <a href="home.php">Naalya Town, Metroplex-Kiwatule  </a></h4>
+	<p>Hours of Operation&nbsp;&nbsp;&bull;&nbsp;&nbsp;Mon - Sun: 10:00 am - 12:00 am</p>
+	
+	<p>&copy; Copyright 2021 PURGE TRANSPORTATION AND LOGISTICS | All Rights Reserved +256 786964345 ;<br /></p>
+</div>
+</body>
+
+
+</html>
+
+</body>
+</html>
--- save_user.php
+++ save_user.php
@@ -1,6 +1,6 @@
 <?php
 require('config.php');
-php include 'header.php'; ?>
+
 
 
 $pName = $_POST['person_name'];
